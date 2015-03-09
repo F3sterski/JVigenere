4942ebf313303ea1ac032177e1a282e6f29b6e00
Adam <adam.wojciech.zielke@gmail.com>
2015-03-09 14:57:32 +0100

    count class

--- JVigenere/src/count.java
+++ JVigenere/src/count.java
@@ -1,4 +1,26 @@
 
 public class count {
+	private int value;
+	private String substring;
+	
+	count(String a,int b){
+		setSubstring(a);
+		setValue(b);
+	}
 
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public String getSubstring() {
+		return substring;
+	}
+
+	public void setSubstring(String substring) {
+		this.substring = substring;
+	}
 }
